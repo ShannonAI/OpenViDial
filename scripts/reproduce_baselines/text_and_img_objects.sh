6102ba22fbf0a9981b95177b05b1cb1ea1244cd6
mengyuxian <mengyuxian@shannonai.com>
2020-12-30 20:16:34 +0800

    FEA/reproduce baseline3

---
+++ scripts/reproduce_baselines/text_and_img_objects.sh
@@ -0,0 +1,52 @@
+# hyper-params
+LR=2e-4
+DROPOUT=0.3
+LAYER=3
+WARMUP=6000
+MAX_OBJ=20
+
+# directory to save models
+MODEL_DIR="/data/yuxian/train_logs/video/images/layer${LAYER}_lr${LR}_bsz128_drop${DROPOUT}_warmup${WARMUP}"
+mkdir -p $MODEL_DIR
+# data directory
+DATA_DIR="/data/yuxian/datasets/new-video/preprocessed_data"
+
+
+fairseq-train \
+  --save-dir $MODEL_DIR \
+  --user-dir video_dialogue_model \
+  --task video-dialogue \
+  --img-type "objects" \
+  --max-obj $MAX_OBJ \
+  --data-dir $DATA_DIR \
+  --arch baseline-obj-transformer \
+  --encoder-layers $LAYER \
+  --decoder-layers $LAYER \
+  --encoder-embed-dim 512 \
+  --share-decoder-input-output-embed \
+  --dropout $DROPOUT \
+  --optimizer adam \
+  --max-tokens 8000 \
+  --batch-size 128 \
+  --adam-betas "(0.9,0.999)" \
+  --reset-optimizer \
+  --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates $WARMUP \
+  --lr $LR \
+  --max-epoch 20 \
+  --keep-last-epochs 5 \
+  --ddp-backend=no_c10d \
+  --num-workers 24  # Reading img-objects mmap-file could be very slow, so we recommend to use a large number of workers.
+
+
+# generate system predictions to OUTPUT
+OUTPUT="${MODEL_DIR}/gen.out"
+python generate.py \
+  --user-dir video_dialogue_model \
+  --task video-dialogue \
+  --img-type $TYPE \
+  --data-dir $DATA_DIR \
+  --path $MODEL_PATH \
+  --beam 5 \
+  --batch-size 32 \
+  --remove-bpe \
+  --gen-subset "test" >$OUTPUT 2>&1 & tail -f $OUTPUT 2>&1
