179833b9b97aa361f13bb36c3af34f35e7b7d407
mengyuxian <mengyuxian@shannonai.com>
2020-12-30 19:40:55 +0800

    FEA/reproduce text baseline

--- scripts/reproduce_text.sh
+++ scripts/reproduce_text.sh
@@ -1,18 +1,17 @@
-export CUDA_VISIBLE_DEVICES=1
-
+# hyper-params
 LR=3e-4
 DROPOUT=0.3
 LAYER=3
 WARMUP=6000
 
+# directory to save models
+MODEL_DIR="/data/yuxian/train_logs/video/text_only/layer${LAYER}_lr${LR}_bsz128_drop${DROPOUT}_warmup${WARMUP}"
+# data directory
+DATA_DIR="/data/yuxian/datasets/new-video/preprocessed_data"
 
-LOG_DIR="/userhome/yuxian/train_logs/video/text_only/layer${LAYER}_lr${LR}_bsz128_drop${DROPOUT}_warmup${WARMUP}"
-DATA_DIR="/userhome/yuxian/data/video/preprocessed_data"
 
-
-#fairseq-train \
-python train.py \
-  --save-dir $LOG_DIR \
+fairseq-train \
+  --save-dir $MODEL_DIR \
   --user-dir video_dialogue_model \
   --task video-dialogue \
   --img-type "features" \
@@ -30,6 +29,21 @@ python train.py \
   --reset-optimizer \
   --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates $WARMUP \
   --lr $LR \
-  --max-epoch 20 \
+  --max-epoch 1 \
   --keep-last-epochs 5 \
   --ddp-backend=no_c10d
+
+
+# generate system predictions to OUTPUT
+OUTPUT="${MODEL_DIR}/gen.out"
+python generate.py \
+  --user-dir video_dialogue_model \
+  --print-attention \
+  --task video-dialogue \
+  --img-type $TYPE \
+  --data-dir $DATA_DIR \
+  --path $MODEL_PATH \
+  --beam 5 \
+  --batch-size 32 \
+  --remove-bpe \
+  --gen-subset "test" >$OUTPUT 2>&1 & tail -f $OUTPUT 2>&1
