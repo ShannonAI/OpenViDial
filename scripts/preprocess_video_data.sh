33351068b5187b39479e9f4d1493b1c777c613a9
mengyuxian <mengyuxian@shannonai.com>
2020-12-31 20:52:18 +0800

    FEA/clean codebase

--- scripts/preprocess_video_data.sh
+++ scripts/preprocess_video_data.sh
@@ -1,12 +1,6 @@
-wangshuh#ORIGIN_DIR="sample_data/origin_data"
-#OUTPUT_DIR="sample_data/preprocessed_data"
 
-#ORIGIN_DIR="/data/yuxian/datasets/video/origin_data"
-#OUTPUT_DIR="/data/yuxian/datasets/video/preprocessed_data"
-#ORIGIN_DIR="/userhome/yuxian/data/video/origin_data"
-#OUTPUT_DIR="/userhome/yuxian/data/video/preprocessed_data"
-ORIGIN_DIR="/data/wangshuhe/video/origin_dir/1"
-OUTPUT_DIR="/data/wangshuhe/video/pre"
+ORIGIN_DIR="/userhome/yuxian/data/video/origin_data"
+OUTPUT_DIR="/userhome/yuxian/data/video/preprocessed_data"
 
 
 python preprocess_video_data.py \
@@ -23,4 +17,4 @@ python preprocess_video_data.py \
 python preprocess_video_data.py \
 --origin-dir $ORIGIN_DIR \
 --output-dir $OUTPUT_DIR \
---split "test" 
+--split "test"
